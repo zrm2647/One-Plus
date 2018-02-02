c4726c42a1cae432bc248d546b06e967c7dc0420
Corinne Green <cmg7063@g.rit.edu>
2018-02-01 19:48:00 -0500

    basic enemy follow
    BEES

---
+++ One Plus/Assets/Scripts/FollowingEnemy.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowingEnemy : MonoBehaviour
+{
+    public GameObject target;
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player");
+        speed = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = target.transform.position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90f;
+        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, q, 180);
+        transform.Translate(Vector3.up * Time.deltaTime * speed);
+    }
+}
